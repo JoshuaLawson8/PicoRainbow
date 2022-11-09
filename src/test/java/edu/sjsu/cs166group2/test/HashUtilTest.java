8282a9d50804f36411559c411db1c37b0e8976e9
Joshua Lawson <joshualawson8@gmail.com>
2022-11-09 14:41:44 -0800

    fixing hashUtilTest

--- src/test/java/edu/sjsu/cs166group2/test/HashUtilTest.java
+++ src/test/java/edu/sjsu/cs166group2/test/HashUtilTest.java
@@ -3,8 +3,6 @@ package edu.sjsu.cs166group2.test;
 import edu.sjsu.cs166group2.util.HashUtil;
 
 import junit.framework.TestCase;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 import java.nio.charset.StandardCharsets;
 
@@ -21,15 +19,14 @@ public class HashUtilTest extends TestCase {
         }
     }
 
-    @Test
     public void testingPassword() {
         assertEquals(testUtil.hash256("password"), "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8");
     }
-    @Test
+
     public void testNumbers() {
         assertEquals(testUtil.hash256("1234567890"),"c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646");
     }
-    @Test
+
     public void testASCII() {
         assertEquals(testUtil.hash256(" !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"),
                 "58a43742aa05762489a8b0662ff8f8d1fc20444e0af90e8abc6bef14aa57ef1b");
